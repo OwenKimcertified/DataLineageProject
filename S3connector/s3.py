acf76981ec260f39c7ad7c5464c82caa67e8e670
Owenkimcertified <ehdrb3309@naver.com>
2025-05-12 23:19:18 +0900

    S3

--- S3connector/s3.py
+++ S3connector/s3.py
@@ -9,6 +9,7 @@ key2 = os.getenv("AWS_SECRET_KEY")
 key3 = os.getenv("REGION")
 key4 = os.getenv("BUCKET_NAME")
 
+# Minio, GCS 등을 사용한다면 의존성 교체가 용이하도록 ABC 고려
 
 class S3object:
     def __init__(self, accessKey, secretKey, region):
@@ -19,24 +20,33 @@ class S3object:
                 region_name           = region
             )
 
-    def list_buckets(self):
+    def list_buckets(self) -> str:
+        """bucket 리스트를 출력"""
         response = self.s3.list_buckets()
         return [b["Name"] for b in response.get("Buckets", [])]
 
-    def list_objects(self, bucket, prefix: str = "~/"):
-        """prefix는 /로 끝나야 함.
-           ex : abc/
+    def list_objects(self, bucket, prefix: str = "~/") -> list[dict]:
+        """
+           bucket의 특정 폴더를 조회
+           prefix는 /로만 끝나야 함.
+           ex : abc/ -> 버킷 내 폴더
         """
         paginator = self.s3.get_paginator("list_objects_v2")
         pages = paginator.paginate(Bucket = bucket, Prefix = prefix)
+        result = []
         for page in pages:
             for obj in page.get("Contents", []):
-                yield obj
-                    
-bucket = S3object(key1, key2, key3)
-rawData = bucket.list_objects('lake.evt', 'raw/')
+                result.append(obj)
+                
+        return result
 
-print(f"bucket name : {bucket.list_buckets()}")
 
-for i in rawData:
-    print(f"data list : {i['Key'], i['Size']}")
+### 사용 예시        
+# bucket = S3object(key1, key2, key3)
+# rawData = bucket.list_objects('lake.evt', "raw/")
+
+# print(f"bucket name : {bucket.list_buckets()}")
+# print(rawData)
+# # 자세히 보기
+# for i in rawData:
+#     print(f"data list : {i['Key'], i['Size']}")
